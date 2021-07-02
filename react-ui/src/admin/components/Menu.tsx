6970208846e930913d63a7c234a36c6be3d406aa
Thiago Sena <lord.sena@gmail.com>
2021-07-02 00:04:46 -0300

    feat: init react project

---
+++ react-ui/src/admin/components/Menu.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Menu = () => {
+   return (
+      <nav className="col-md-2 d-none d-md-block bg-light sidebar">
+         <div className="sidebar-sticky">
+            <ul className="nav flex-column">
+               <li className="nav-item">
+                  <a className="nav-link" href="/">
+                     <span data-feather="home"/>
+                     Home
+                  </a>
+               </li>
+               <li className="nav-item">
+                  <a className="nav-link active" href="#">
+                     <span data-feather="shopping-cart"/>
+                     Products <span className="sr-only">(current)</span>
+                  </a>
+               </li>
+            </ul>
+         </div>
+      </nav>
+   );
+};
+
+export default Menu;
